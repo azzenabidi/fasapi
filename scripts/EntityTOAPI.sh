125d2a6c09fb966b61054a2eef9fdf2c5350397a
azzenovic <azzen.abidi@gmail.com>
2017-01-02 03:25:48 +0100

    Add Daemon scripts

---
+++ scripts/EntityTOAPI.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+#This script converts doctrine entities to full REST API
+
+# Author: Azzen Abidi
+
+# Contact: azzen.abidi@gmail.com
---
+++ scripts/SQLTOAPI.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+#This script converts an sql file to full API 
+
+# Author: Azzen Abidi
+
+# Contact: azzen.abidi@gmail.com
